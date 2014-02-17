702984a8b92ae00685790eff2c22762c4da80b3f
nelsonic <nodecoder@gmail.com>
2014-02-17 19:24:49 +0000

    updates comments in cash.js for JSDoc

--- cash.js
+++ cash.js
@@ -3,9 +3,9 @@ C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
 /**
  * getChange returns and Array containing the values of notes & coins
  * equivalent to the change for a given transaction
- * @param totalPayable the amount payable for a transaction
- * @param cashPaid  the amount the customer hands over to pay
- * return [500,20,5] the array equivalent of the change
+ * @param {Number}  totalPayable the amount payable for a transaction
+ * @param {Number}  cashPaid  the amount the customer hands over to pay
+ * @returns {Array} [500,20,5] the array equivalent of the change
  */
 
 C.getChange = function (totalPayable, cashPaid) {
